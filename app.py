12c4906aedea2df26d7e90b9c225b47f3351e7b9
njNagaraj <83235902+njNagaraj@users.noreply.github.com>
2024-01-09 15:54:59 +0000

    added layout_template.html to understand the concept of layout inheritance

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+  ]
+
 @app.route('/')
 @app.route('/home')
 def home_page():
-  return render_template('home.html')
+  return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about_page():
-  return render_template('about.html')
+  return render_template('about.html', title = 'About')
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', debug = True)
