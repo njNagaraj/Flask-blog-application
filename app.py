4d2bc87ee509eb444675947752318c71e7974566
njNagaraj <83235902+njNagaraj@users.noreply.github.com>
2024-01-09 15:23:53 +0000

    Added separate html pages using render template

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+@app.route('/home')
+def home_page():
+  return render_template('home.html')
+
+@app.route('/about')
+def about_page():
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', debug = True)
