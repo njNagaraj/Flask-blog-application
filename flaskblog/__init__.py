b788cfcae778bba45fbf2ff9f754c97a8fe9f617
njNagaraj <83235902+njNagaraj@users.noreply.github.com>
2024-01-16 15:18:57 +0000

    added forgot password page and reset password page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 #importing required class from flask
 
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -15,7 +17,12 @@ app.app_context().push()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'
-login_manager.login_message_category = 'info'\
-
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+mail = Mail(app)
 
 from flaskblog import routes
