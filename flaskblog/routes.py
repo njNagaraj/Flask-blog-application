472eadd6b3e7b2d691025eca82fd90bddc3f6b71
njNagaraj <83235902+njNagaraj@users.noreply.github.com>
2024-01-16 12:55:01 +0000

    User can update and delete their post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 import re
-from flask import flash, redirect, render_template, url_for, request
+from flask import flash, redirect, render_template, url_for, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -99,4 +99,40 @@ def new_post():
     db.session.commit()
     flash('Posted', 'success')
     return redirect(url_for('home_page'))
-  return render_template('create_post.html', title='New Post', form=form)
+  return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+  post = Post.query.get_or_404(post_id)
+  return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+    abort(403)
+  form = PostForm()
+  if form.validate_on_submit():
+    post.title = form.title.data
+    post.content = form.content.data
+    db.session.commit()
+    flash('Your post has been updated!', 'success')
+    return redirect(url_for('post', post_id=post.id))
+  elif request.method == 'GET':
+    form.title.data = post.title
+    form.content.data = post.content
+  return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+    abort(403)
+  db.session.delete(post)
+  db.session.commit()
+  flash('Your post has been deleted!', 'success')
+  return redirect(url_for('home_page'))
