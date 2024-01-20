3ad4227074f105f90b6c4858fd3889f48afbe16d
njNagaraj <83235902+njNagaraj@users.noreply.github.com>
2024-01-20 14:35:09 +0000

    organized code with blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY = '76r5uvveyh64evyu4veu4'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'xcvkp@example.com'
+    MAIL_PASSWORD = 'yourpassword'
